aa78e4b0035c43c1870530f6d6bc8d0862d26b07
MalMWare <malkelleymartin@gmail.com>
2023-05-16 19:02:13 -0700

    debugging

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,10 +1,17 @@
 const router = require('express').Router()
 const db = require("../models")
+const bcrypt = require('bcrypt')
 
 const { User } = db
 
 router.post('/', async (req, res) => {
-    const user = await User.create(req.body)
+    const { password, ...rest } = req.body
+    const passHash = await bcrypt.hash(password, 10)
+    console.log(password, passHash)
+    const user = await User.create({
+        ...rest,
+        passwordDigest: passHash
+    })
     res.json(user)
 })
 
---
+++ backend/migrations/20230514204901-add-user-password.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return await  queryInterface.addColumn('users', 'password_digest', {
+      type: Sequelize.DataTypes.STRING
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return await queryInterface.removeColumn('users', 'password_digest')
+  }
+};
